0c081c1195973dab988212fab0e5bba84dd1564e
sahil choudhary <56938234+SahilChoudhary998@users.noreply.github.com>
2022-07-11 22:11:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 
+'react-router-dom';
+import Navbar from './components/Navbar/Navbar'
+import SignIn from './pages/Navbar/SignIn';
 function App() {
   return (
-    <div style={{backgroundColor: 'lightsalmon'}}>
-      <h2>Welcome to TRAV3L3R homepage!</h2>
+    <Router>
+    <Navbar />
+    <div className="container">
+    <Routes>
+      {/* <Route path='/' exact component={Home} />
+      <Route path='/Search' component={Search} /> */}
+      {/* <Route path='/SignIn' component={SignIn} /> */}
+      <Route exact path="/SignIn" element={<SignIn />} />
+      {/* <Route path='/SignUp' component={SignUp} /> */}
+    </Routes>
     </div>
+  </Router>
+  
+
+
   );
 }
-
 export default App;
+
