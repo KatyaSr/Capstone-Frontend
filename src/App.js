a9e742a13aced6bb00f378ad887661ea8cb4c393
sahil choudhary <56938234+SahilChoudhary998@users.noreply.github.com>
2022-07-21 00:53:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import {BrowserRouter as Router, Routes, Route} from
 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import SignIn from './pages/Navbar/SignIn';
+import SignUp from './pages/Navbar/SignUp';
+import Profile from './pages/Navbar/Profile';
+import Result from './pages/Navbar/Result';
+import Forgotpassword from './pages/Navbar/Forgotpassword';
+import Search from './pages/Navbar/Search';
 function App() {
   return (
     <Router>
     <Navbar />
+    <div className='columnContainer'>
     <div className="container">
     <Routes>
-      {/* <Route path='/' exact component={Home} />
-      <Route path='/Search' component={Search} /> */}
-      <Route exact path="/SignIn" element={<SignIn />} />
-      {/* <Route path='/SignUp' component={SignUp} /> */}
+      {/* <Route path='/' exact component={Home} />*/}
+       <Route exact path="/SignIn" element={<SignIn />} />
+       <Route exact path='/SignUp' element={<SignUp/>} />
+       <Route exact path='/Profile' element={<Profile/>} />
+       <Route exact path='/Search' element={<Search/>} /> 
+       <Route exact path='/Result' element={<Result/>} />
+       <Route exact path='/Forgotpassword' element={<Forgotpassword/>} />
     </Routes>
+    </div>
     </div>
   </Router>
   
