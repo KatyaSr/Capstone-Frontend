0e49715a3416cf33f29776a3957623e27c7f1e00
sahil choudhary <56938234+SahilChoudhary998@users.noreply.github.com>
2022-07-21 00:49:17 -0400

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1,1 +1,19 @@
+import React from "react";
+import {useParams, useNavigate} from "react-router-dom";
 
+function Profile() {
+    let navigate = useNavigate();
+    let { username } = useParams();
+    return <div className="columnContainer"> 
+    THIS IS profile PAGE OF {username};
+    <button onClick = {() =>navigate('/Home')}>
+        Home
+    </button>
+    <div>
+        Contact:
+        
+    </div>
+    </div>;
+}
+
+export default Profile;
